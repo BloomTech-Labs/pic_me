28f0b9e587394f377e80a82b9ae2ba7536fe4c90
Elly <ellyhahn@gmail.com>
2018-05-24 11:12:21 -0700

    fix componenents error

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,74 +1,77 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { reduxForm, Field } from 'redux-form';
-import { Button } from 'reactstrap';
+import { login, resetErrors } from '../actions';
 
-class Signin extends Component {
-
-    submitFormHandler = ({ username, password }) => {
-      this.props.login(username, password, this.props.history);
-    };
-  
-    render() {
-      return (
-        <div className="Signup">
-  
-          <form
-            className="Signup__form"
-            onSubmit={this.props.handleSubmit(this.submitFormHandler)}
-          >
-            {/* <div className="Signup__form"> */}
-            <div className="SignupDescription">
-              {this.props.authenticating
-                ? 'Logging in..'
-                : this.props.error === '' ? 'Log in' : this.props.error}
-            </div>
-  
-            <div className="SignupForm">
-              <fieldset>
-                <Field
-                  className="InputFields"
-                  name="username"
-                  component="input"
-                  type="text"
-                  placeholder="username"
-                />
-              </fieldset>
-  
-              <fieldset>
-                <Field
-                  className="InputFields"
-                  name="password"
-                  component="input"
-                  type="password"
-                  placeholder="password"
-                />
-              </fieldset>
-  
-              <Button className="SignupForm__button" action="submit">
-                Sign In
-              </Button>
-  
-              <NavLink className="SignupForm__NavLink" to="/signup">
-                <div>Sign Up for Free</div>
-              </NavLink>
-            </div>
-          </form>
-        </div>
-      );
-    }
+class Login extends Component {
+  componentWillMount() {
+    this.props.resetErrors();
   }
 
-//   const mapStateToProps = state => {
-//     return {
-//       authenticating: state.auth.authenticating,
-//       error: state.auth.error,
-//     };
-//   };
+  submitFormHandler = ({ 
+    email, 
+    password, 
+  }) => {
+    this.props.login(email, password, this.props.history);
+  };
+  
+  render() {
+    return (
+      <div className="Signup">
 
-//   Signin = connect(mapStateToProps, { signin, resetErrors })(Signin);
+        <form
+          className="Signup__form"
+          onSubmit={this.props.handleSubmit(this.submitFormHandler)}
+        >
+          <div className="SignupDescription">
+            {this.props.authenticating
+              ? 'Logging in..'
+              : this.props.error === '' ? 'Log in' : this.props.error}
+          </div>
 
-  export default reduxForm({
-    form: 'signin',
-    fields: ['username', 'password'],
-  })(Signin);
+          <div className="SignupForm">
+            <fieldset>
+              <Field
+                className="InputFields"
+                name="email"
+                component="input"
+                type="text"
+                placeholder="email"
+              />
+            </fieldset>
+
+            <fieldset>
+              <Field
+                className="InputFields"
+                name="password"
+                component="input"
+                type="password"
+                placeholder="password"
+              />
+            </fieldset>
+
+            <button className="SignupForm__button" action="submit">
+              Log in
+            </button>
+
+            <NavLink className="SignupForm__NavLink" to="/signup">
+              Don't have an account? Sign up
+            </NavLink>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    authenticating: state.auth.authenticating,
+    error: state.auth.error,
+  };
+};
+Login = connect(mapStateToProps, { login, resetErrors })(Login);
+export default reduxForm({
+  form: 'login',
+  fields: ['email', 'password'],
+})(Login);
