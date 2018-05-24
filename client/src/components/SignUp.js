98e354413f62e176cffbc59679dd44cb2003dfd5
Elly <ellyhahn@gmail.com>
2018-05-24 11:07:54 -0700

    fix login pages

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { reduxForm, Field } from 'redux-form';
-import { Button } from 'reactstrap';
+import { register, resetErrors } from '../actions';
 
 class Signup extends Component {
+  componentWillMount() {
+    this.props.resetErrors();
+  }
 
-  submitFormHandler = ({ username, password, confirmPassword }) => {
+  submitFormHandler = ({ email, password, confirmPassword, firstName, lastName }) => {
     this.props.register(
-      username,
+      email,
       password,
       confirmPassword,
+      firstName,
+      lastName, 
       this.props.history,
     );
   };
@@ -17,77 +23,92 @@ class Signup extends Component {
   render() {
     return (
       <div className="Signup">
-
         <form
           className="Signup__form"
           onSubmit={this.props.handleSubmit(this.submitFormHandler)}
+
         >
-          {/* <div className="Signup__form"> */}
-          <div className="SignupDescription">
-            {this.props.authenticating
-              ? 'Signing up..'
-              : this.props.error === ''
-                ? 'Sign up for an account'
-                : this.props.error}
-          </div>
+        <div className="SignupDescription">
+          {this.props.authenticating
+            ? 'Signing up..'
+            : this.props.error === ''
+              ? 'Sign up for an account'
+              : this.props.error}
+        </div>
 
-          <div className="SignupForm">
-            <fieldset>
-                <Field
-                    className="InputFields"
-                    name="username"
-                    component="input"
-                    type="text"
-                    placeholder="username"
-                />
-            </fieldset>
+        <div>
+          <fieldset>
+            <Field
+              className="InputFields"
+              name="email"
+              component="input"
+              type="text"
+              placeholder="email"
+            />
+          </fieldset>
 
+          <fieldset>
+            <Field
+              className="InputFields"
+              name="password"
+              component="input"
+              type="password"
+              placeholder="password"
+            />
+          </fieldset>
 
-            <fieldset>
-                <Field
-                    className="InputFields"
-                    name="password"
-                    component="input"
-                    type="password"
-                    placeholder="password"
-                />
-            </fieldset>
-            <fieldset>
-                <Field
-                    className="InputFields"
-                    name="confirmPassword"
-                    component="input"
-                    type="password"
-                    placeholder="confirm password"
-                />
-            </fieldset>
-            <div>
-            By clicking Sign up, I agree to the Terms of Service and Privacy Policy.
-            </div>
-            <Button className="SignupForm__button" action="submit">
-              Sign Up
-            </Button>
+          <fieldset>
+            <Field
+              className="InputFields"
+              name="confirmPassword"
+              component="input"
+              type="password"
+              placeholder="confirm password"
+            />
+          </fieldset>
 
-            <NavLink className="SignupForm__NavLink" to="/signin">
-              <div>Have an account? Sign In</div>
-            </NavLink>
-          </div>
+          <fieldset>
+            <Field
+              className="InputFields"
+              name="firstName"
+              component="input"
+              type="text"
+              placeholder="first name"
+            />
+          </fieldset>
+
+          <fieldset>
+            <Field
+              className="InputFields"
+              name="lastName"
+              component="input"
+              type="text"
+              placeholder="last name"
+            />
+          </fieldset>
+
+          <button className="SignupForm__button" action="submit">
+            Sign up
+          </button>
+
+          <NavLink className="SignupForm__NavLink" to="/login">
+            Have an account? Log in
+          </NavLink>
+        </div>
         </form>
       </div>
     );
   }
 }
 
-// const mapStateToProps = state => {
-//   return {
-//     authenticating: state.auth.authenticating,
-//     error: state.auth.error,
-//   };
-// };
+const mapStateToProps = state => ({
+  authenticating: state.auth.authenticating,
+  error: state.auth.error,
+});
 
-// Signup = connect(mapStateToProps, { register, resetErrors })(Signup);
+Signup = connect(mapStateToProps, { register, resetErrors })(Signup);
 
 export default reduxForm({
   form: 'signup',
-  fields: ['username', 'password', 'confirmPassword'],
+  fields: ['email', 'password', 'confirmPassword', 'firstName', 'lastName'],
 })(Signup);
