3d0a1830971e87f36b67cca6551274135a00c6bf
Samuel Cha <sunchipssam@gmail.com>
2018-06-01 08:58:28 -0700

    Added user field for payment

--- client/src/components/payment/CheckoutForm.js
+++ client/src/components/payment/CheckoutForm.js
@@ -1,110 +1,112 @@
 // CheckoutForm.js
 import React from 'react';
 import {
-  injectStripe,
-  CardElement,
-  // CardNumberElement,
-  // CardExpiryElement,
-  // CardCVCElement,
-  // PostalCodeElement,
+	injectStripe,
+	CardElement,
+	// CardNumberElement,
+	// CardExpiryElement,
+	// CardCVCElement,
+	// PostalCodeElement,
 } from 'react-stripe-elements';
 import { connect } from 'react-redux';
 import { sendPayment } from '../../actions';
 
 const handleBlur = () => {
-  console.log('[blur]');
+	console.log('[blur]');
 };
 const handleChange = change => {
-  console.log('[change]', change);
+	console.log('[change]', change);
 };
 // const handleClick = () => {
 // console.log('[click]');
 // };
 const handleFocus = () => {
-  console.log('[focus]');
+	console.log('[focus]');
 };
 const handleReady = () => {
-  console.log('[ready]');
+	console.log('[ready]');
 };
 
 const createOptions = fontSize => {
-  return {
-    style: {
-      base: {
-        fontSize,
-        color: '#424770',
-        letterSpacing: '0.025em',
-        fontFamily: 'Source Code Pro, monospace',
-        '::placeholder': {
-          color: '#aab7c4',
-        },
-      },
-      invalid: {
-        color: '#9e2146',
-      },
-    },
-  };
+	return {
+		style: {
+			base: {
+				fontSize,
+				color: '#424770',
+				letterSpacing: '0.025em',
+				fontFamily: 'Source Code Pro, monospace',
+				'::placeholder': {
+					color: '#aab7c4',
+				},
+			},
+			invalid: {
+				color: '#9e2146',
+			},
+		},
+	};
 };
 
 class CheckoutForm extends React.Component {
-  handleSubmit = ev => {
-    // We don't want to let default form submission happen here, which would refresh the page.
-    ev.preventDefault();
+	handleSubmit = ev => {
+		// We don't want to let default form submission happen here, which would refresh the page.
+		ev.preventDefault();
 
-    // Within the context of `Elements`, this call to createToken knows which Element to
-    // tokenize, since there's only one in this group.
-    this.props.stripe.createToken({ name: 'Jenny Rosen' }).then(({ token }) => {
-      console.log('Received Stripe token:', token);
-      this.props.sendPayment(token.id);
-    });
+		// Within the context of `Elements`, this call to createToken knows which Element to
+		// tokenize, since there's only one in this group.
+		this.props.stripe
+			.createToken({ name: `${this.props.user}` })
+			.then(({ token }) => {
+				console.log('Received Stripe token:', token);
+				this.props.sendPayment(token.id);
+			});
 
-    // However, this line of code will do the same thing:
-    // this.props.stripe.createToken({type: 'card', name: 'Jenny Rosen'});
-  };
+		// However, this line of code will do the same thing:
+		// this.props.stripe.createToken({type: 'card', name: 'Jenny Rosen'});
+	};
 
-  render() {
-    return (
-      <form
-        onSubmit={this.handleSubmit}
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <label>
-          Card details
-          <CardElement
-            onBlur={handleBlur}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onReady={handleReady}
-            {...createOptions(this.props.fontSize)}
-          />
-        </label>
-        {/* <CardElement /> */}
-        {/* <CardNumberElement /> */}
-        {/* <CardExpiryElement /> */}
-        {/* <CardCVCElement /> */}
-        {/* <PostalCodeElement /> */}
-        <button
-          style={{
-            alignSelf: 'center',
-            width: '100px',
-          }}
-        >
-          Pay
-        </button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form
+				onSubmit={this.handleSubmit}
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+				}}
+			>
+				<label>
+					Card details
+					<CardElement
+						onBlur={handleBlur}
+						onChange={handleChange}
+						onFocus={handleFocus}
+						onReady={handleReady}
+						{...createOptions(this.props.fontSize)}
+					/>
+				</label>
+				{/* <CardElement /> */}
+				{/* <CardNumberElement /> */}
+				{/* <CardExpiryElement /> */}
+				{/* <CardCVCElement /> */}
+				{/* <PostalCodeElement /> */}
+				<button
+					style={{
+						alignSelf: 'center',
+						width: '100px',
+					}}
+				>
+					Pay
+				</button>
+			</form>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  return {
-    //
-  };
+	return {
+		user: state.auth.user,
+	};
 };
 
 export default connect(mapStateToProps, { sendPayment })(
-  injectStripe(CheckoutForm),
+	injectStripe(CheckoutForm),
 );
