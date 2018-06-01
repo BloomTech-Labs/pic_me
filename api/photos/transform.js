f477bb555e3ac7e4c07cea767521df650fb71d95
Samuel Cha <sunchipssam@gmail.com>
2018-05-31 17:07:03 -0700

    Added s3 perm
    Per Julian

--- api/photos/transform.js
+++ api/photos/transform.js
@@ -17,58 +17,61 @@ const awsAccessKey = process.env.AWS_KEY;
 const awsSecretKey = process.env.AWS_SECRET;
 
 aws.config.update({
-  secretAccessKey: awsSecretKey,
-  accessKeyId: awsAccessKey,
-})
+	secretAccessKey: awsSecretKey,
+	accessKeyId: awsAccessKey,
+});
 
 let s3 = new aws.S3();
 
 const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.BUCKET,
-    // acl permissions 'public-read'
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    // should transform
-    shouldTransform: true,
-    // transforms
-    transforms: [{
-      id: 'original',
-      key: function (req, file, cb) {
-        let fileSplit = file.originalname.split('.')
+	storage: multerS3({
+		s3: s3,
+		bucket: process.env.BUCKET,
+		//acl
+		permissions: 'public-read',
+		contentType: multerS3.AUTO_CONTENT_TYPE,
+		// should transform
+		shouldTransform: true,
+		// transforms
+		transforms: [
+			{
+				id: 'original',
+				key: function(req, file, cb) {
+					let fileSplit = file.originalname.split('.');
 
-        let filename = fileSplit.slice(0, fileSplit.length - 1)
-        filename.push(Date.now())
-        filename = filename.join('_') + '.' + fileSplit[fileSplit.length - 1]
+					let filename = fileSplit.slice(0, fileSplit.length - 1);
+					filename.push(Date.now());
+					filename = filename.join('_') + '.' + fileSplit[fileSplit.length - 1];
 
-        cb(null, filename)
-      },
-      transform: function (req, file, cb) {
-        cb(null, sharp().resize(293, 293))
-      }
-    }],
-    metadata: function(req, file, cb) {
-      cb(null, {fieldName: 'images', fieldName: 'tags'});
-    },
-  })
-})
+					cb(null, filename);
+				},
+				transform: function(req, file, cb) {
+					cb(null, sharp().resize(293, 293));
+				},
+			},
+		],
+		metadata: function(req, file, cb) {
+			cb(null, { fieldName: 'images', fieldName: 'tags' });
+		},
+	}),
+});
 
 server.get('/', (req, res) => {
-  console.log('Photo upload endpoint');
-  res.send({ message: 'this route worked.' });
+	console.log('Photo upload endpoint');
+	res.send({ message: 'this route worked.' });
 });
 
 server.post('/upload', upload.array('images'), (req, res, next) => {
-  const uploads = req.files
-  uploads.forEach(i => {    
-    let newImage = new image();
-    // using shift() to make postman associate proper tags with image instead
-    // of just appending all tags from uploads as an array for each upload
-    newImage.tags = req.body.tags.shift(); 
-    newImage.url = i.transforms[0].location;
-    newImage.save();
-  })
-  res.json(`Uploaded ${req.files.length} files!`);
-})  
+	const uploads = req.files;
+	uploads.forEach(i => {
+		let newImage = new image();
+		// using shift() to make postman associate proper tags with image instead
+		// of just appending all tags from uploads as an array for each upload
+		newImage.tags = req.body.tags.shift();
+		newImage.url = i.transforms[0].location;
+		newImage.save();
+	});
+	res.json(`Uploaded ${req.files.length} files!`);
+});
 
 module.exports = server;
