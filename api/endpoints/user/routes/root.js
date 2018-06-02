2e1078cf7067f67088e9919b20dd6bc474742690
Samuel Cha <sunchipssam@gmail.com>
2018-06-01 17:09:46 -0700

    Added settings and root into sep routes

--- api/endpoints/user/routes/root.js
+++ api/endpoints/user/routes/root.js
@@ -2,10 +2,21 @@ const router = require('express').Router();
 
 const { debug } = require('../../../../dev');
 
-const send = require('../../../helpers/send');
+/**
+ * controller that interacts with the users table in database
+ */
+const userCTR = require('../../../users/controller');
 
 /**
- * /api/users (`/`)
+ * general helper functions for all api endpoints
+ */
+const validate = require('../../../helpers/validate/index');
+const sanitize = require('../../../helpers/sanitize');
+const authenticate = require('../../../helpers/authenticate');
+const r = require('../../../helpers/responses');
+
+/**
+ * /api/users
  * - GET:    used for debug only
  * - POST:   creates a new user
  * - PUT:    edits the user's non-sensitive info
@@ -14,14 +25,19 @@ const send = require('../../../helpers/send');
 router
 	.route('/')
 	.get((req, res) => {
-		debug ? send(res, 200, { users: `running` }) : null;
+		debug ? response.send(res, 200, { users: `running` }) : null;
 	})
+
 	/**
 	 * POST /api/users
 	 *
 	 * creates a new user
 	 *
-	 * expects (in req.body):
+	 * checks the new user's signup info, then
+	 * sanitizes new user's info, then
+	 * saves new user to the database
+	 *
+	 * expects:
 	 * {
 	 *    email: 'my@email.com',
 	 *    password: 'unhashedPassword',
@@ -31,37 +47,92 @@ router
 	 *
 	 * returns:
 	 * {
-	 *
+	 *    user:
+	 *    {
+	 *       email: 'my@email.com',
+	 *       firstName: 'john',
+	 *       lastName: 'doe',
+	 *       balance: 0,
+	 *       nickNames: ['John'],
+	 *       photos: [],
+	 *       uploads: []
+	 *    }
 	 * }
-	 *
 	 */
 	.post(validate.signup, sanitize.user, (req, res) => {
 		userCTR
 			.create(req.newUser)
-			.then(savedUser => send(res, 201, sanitize.response(savedUser)))
-			.catch(err =>
-				send(res, 500, { err, message: `server failed to save new user` }),
-			);
+			.then(savedUser => r.send(res, 201, sanitize.response(savedUser)))
+			.catch(err => r.error(res, err, `server failed to save new user`));
 	})
+
+	/**
+	 * PUT /api/users
+	 *
+	 * edits the user's non-sensitive info
+	 *
+	 * checks for a valid session id, then
+	 * checks the update info for at least one editable field (see below), and
+	 * sanitizes updated info, then
+	 * updates the user info on the database
+	 *
+	 * env config:
+	 * EDITABLE_FIELDS=["firstName","lastName","nickNames"]
+	 *
+	 * expects:
+	 * {
+	 *    user:
+	 *    {
+	 *       firstName: 'Josh',
+	 *       lastName: 'Moe',
+	 *       nickNames: ['Josh', 'MoScho']
+	 *    }
+	 * }
+	 *
+	 * returns:
+	 * {
+	 *    user:
+	 *    {
+	 *       email: 'my@email.com',
+	 *       firstName: 'mary',
+	 *       lastName: 'jane',
+	 *       balance: 0,
+	 *       nickNames: ['Mary', 'MJ'],
+	 *       photos: [],
+	 *       uploads: []
+	 *    }
+	 * }
+	 */
 	.put(authenticate.sid, validate.update, sanitize.update, (req, res) => {
 		userCTR
 			.update(req.user.id, req.editedUser)
-			.then(editedUser => send(res, 200, sanitize.response(editedUser)))
-			.catch(err =>
-				send(res, 500, { err, message: `server failed to edit user` }),
-			);
+			.then(editedUser => r.send(res, 200, sanitize.response(editedUser)))
+			.catch(err => r.error(res, err, `server failed to edit user`));
 	})
+
+	/**
+	 * DELETE /api/users
+	 *
+	 * deletes the user
+	 *
+	 * checks for a valid session id, then
+	 * deletes the user info on the database and
+	 * logs the user out of the session
+	 *
+	 * returns:
+	 * {
+	 *    message: 'user successfully deleted'
+	 * }
+	 */
 	.delete(authenticate.sid, (req, res) => {
 		userCTR
 			.delete(req.user.id)
 			.then(_ => {
 				req.logout();
 
-				send(res, 200, `user successfully deleted`);
+				r.send(res, 200, { message: `user successfully deleted` });
 			})
-			.catch(err =>
-				send(res, 500, { err, message: `server failed to delete user` }),
-			);
+			.catch(err => r.error(res, err, `server failed to delete user`));
 	});
 
 module.exports = router;
