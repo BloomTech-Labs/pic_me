8a268ac024794f727524e7abd0b9c08aedad22ed
Samuel Cha <sunchipssam@gmail.com>
2018-06-04 10:42:30 -0700

    Updated frontend pictures upload

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -332,7 +332,7 @@ export const authenticateUser = history => {
 export const upload = data => {
 	return dispatch => {
 		axios
-			.post(`${ROOT}/photos/upload`)
+			.post(`${ROOT}/pictures/upload`)
 			.then(res => console.log(res))
 			.catch(err => console.error(err));
 	};
