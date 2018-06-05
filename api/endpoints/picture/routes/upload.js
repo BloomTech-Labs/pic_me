d75793a5abce559ed0690b20a98074224639fe73
Julian Kohlman <juliankohlman@gmail.com>
2018-06-05 13:01:57 -0400

    remove some old comments

--- api/endpoints/picture/routes/upload.js
+++ api/endpoints/picture/routes/upload.js
@@ -83,17 +83,6 @@ router
 					r.send(res, 500, { err, message: `server failed to edit user` }),
 				);
 		});
-		// const uploaded = req.files;
-		// uploaded.forEach(i => {
-		// 	let newImage = new image();
-		// 	// using shift() to make postman associate proper tags with image instead
-		// 	// of just appending all tags from uploads as an array for each upload
-		// 	newImage.tags = req.body.tags.shift();
-		// 	newImage.url = i.transforms[0].location;
-		// 	newImage.save();
-		// });
-		// // res.json(`Uploaded ${req.files.length} files!`);
-		// res.send();
 	});
 
 module.exports = router;
