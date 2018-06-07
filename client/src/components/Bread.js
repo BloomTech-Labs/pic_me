261555d9941c9a5ed04ea5b16692d4660c38b815
Elly <ellyhahn@gmail.com>
2018-06-07 14:26:19 -0700

    commit before merge

--- client/src/components/Bread.js
+++ client/src/components/Bread.js
@@ -5,7 +5,7 @@ import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
 const routes = {
   '/': 'Home',
   '/picture_upload': 'Upload',
-  '/picture_browse': 'Browse Other me',
+  '/picture_browse': 'Browse Pic Me',
   '/picture_my_uploads': 'My Uploads',
   '/picture_my_collection': 'My Collection', 
   '/billing': 'Billing',
